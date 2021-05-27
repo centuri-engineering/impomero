fe3f8879346777e4d930610596c70ffd4e1597b5
Guillaume Gay <guillaume@damcb.com>
2021-05-27 15:02:05 +0000

    fixes setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-
 from distutils.core import setup
 
-setup(name='impomero',
-      version='0.1',
-      description='Python auto importer in OMERO',
-      author='Guillaume Gay',
-      author_email='guillaume@damcb.com',
-      url='https://github.com/centuri-engineering/impomero',
-      packages=['auto_import',],
-     )
+setup(
+    name="impomero",
+    version="0.1",
+    description="Python auto importer in OMERO",
+    author="Guillaume Gay",
+    author_email="guillaume@damcb.com",
+    url="https://github.com/centuri-engineering/impomero",
+    packages=[
+        "impomero",
+    ],
+)
