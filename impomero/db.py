17472fe21ff5ff690422ac7e6c1d19ae327fc6c2
Guillaume Gay <guillaume@damcb.com>
2021-07-22 15:28:57 +0000

    bug fix create_table

--- impomero/db.py
+++ impomero/db.py
@@ -8,7 +8,7 @@ def init_db(db_name):
     with sqlite3.connect(db_name) as sql_con:
         sql_con.execute("CREATE TABLE IF NOT EXISTS monitored (date, base_dir)")
         sql_con.execute(
-            """CREATE TABLE IF NOT EXISTS annotated (index, title, created,
-            project, user, comment, tags, accessed, target, fileset, file_path,
-            group, organism, sample, channel_0, id, base_dir)"""
+            """CREATE TABLE IF NOT EXISTS annotated ('index', title, created,
+                project, user, comment, tags, accessed, target, fileset, file_path,
+                'group', organism, sample, channel_0, id, base_dir)"""
         )
